8fabb70faee11e83b7e31fb36c6ffa958cb00f2a
saikat111 <s8856010@gmail.com>
2023-01-02 02:15:24 +0600

    variable

---
+++ Variables.js
@@ -0,0 +1,26 @@
+//use var
+var x  = 5
+var y = 10
+z = x + y;
+console.log(z)
+x = 100
+z = x + y;
+console.log(z)
+//use let
+
+let xx  = 5
+let yy = 10
+zz = xx + yy;
+console.log(zz)
+yy = 100
+zz = xx + yy;
+console.log(zz)
+
+//user const
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+console.log(total)
+//price2 = 11 // here is a error . because we can chnage the constant
+//total = price1 + price2;
+//console.log(total)

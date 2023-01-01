e1a2fd1dd7abb421b5e997a09e4dc1e2713659ec
saikat111 <s8856010@gmail.com>
2023-01-02 03:16:47 +0600

    equals

---
+++ string_match.js
@@ -0,0 +1,11 @@
+var  name1  = "saikat"
+var name2  = "talias"
+console.log(name1 === name2)
+// The === and == operators evaluate whether one value is equal to another
+// == will also checks if the values are equal after one of the values is converted into another type
+console.log("string" === "string"); // -> true
+console.log(1 === 1); // -> true
+console.log(1 === "1"); // -> false
+console.log(1 == "1"); // -> true
+console.log("String" === "string"); // -> false
+console.log("String" == "string"); // -> false

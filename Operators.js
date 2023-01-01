5672ba7b64d4694a3022a1f6389a8e77b08ae22e
saikat111 <s8856010@gmail.com>
2023-01-02 02:19:58 +0600

    data Types

---
+++ Operators.js
@@ -0,0 +1,10 @@
+// Assign the value 5 to x
+let x = 5;
+// Assign the value 2 to y
+let y = 2;
+// Assign the value x + y to z:
+let z = x + y;
+console.log(z)
+let a = 3;
+let xx = (100 + 50) * a;
+console.log(xx)

4ea677ac44d7b1a4e98b69b137cfa852f70898a0
saikat111 <s8856010@gmail.com>
2023-01-02 02:44:48 +0600

    objects

--- objects.js
+++ objects.js
@@ -4,4 +4,5 @@ const person = {
     age: 50,
     eyeColor: "blue"
   };
-  console.log(person)
+  //console.log(person)
+  console.log("The person name is " + person.firstName  + " his age is " + person.age)

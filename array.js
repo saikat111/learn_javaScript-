fbac4f144c5146019bab8a208c66445b26ea709c
saikat111 <s8856010@gmail.com>
2023-01-02 03:28:03 +0600

    arrays

---
+++ array.js
@@ -0,0 +1,10 @@
+var name =["saikat", "jemy", "omor farul"]
+var n = "fff"
+for(let i = 0 ; i <name.length ; i++ ){
+    //console.log(name[i])
+   /* if(name[i] == "saikat"){
+         n == name[i]
+    }
+    console.log(name[i])*/
+
+}
